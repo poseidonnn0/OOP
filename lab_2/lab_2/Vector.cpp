1db65c8d8c746cc333d79af9d32d6c68445e550d
Vitaliy <79005626141@mail.ru>
2023-09-23 21:24:00 +0200

    + cpp

---
+++ lab_2/lab_2/Vector.cpp
@@ -0,0 +1,90 @@
+#include "Vector.h"
+
+namespace vector_func
+{
+    Vector::Vector(const Point& start, const Point& end) // делегируем конструктор для заданных координат точек, если вектор создаётся по точкам
+    {
+        x = end.x - start.x;
+        y = end.y - start.y;
+        z = end.z - start.z;
+    }
+
+    // Операции сложения и вычитания (Переопределяем)
+    Vector operator+(const Vector& v1, const Vector& v2)
+    {
+        return Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+    }
+
+    Vector operator-(const Vector& v1, const Vector& v2)
+    {
+        return Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+    }
+
+    // Получение обратного вектора
+    Vector operator-(const Vector& v)
+    {
+        return Vector(-v.x, -v.y, -v.z);
+    }
+
+    // Построение единичного вектора
+    Vector normalize(const Vector& v) 
+    {
+        double length = std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+        return Vector(v.x / length, v.y / length, v.z / length);
+    }
+
+    // Скалярное произведение
+    double dotProduct(const Vector& v1, const Vector& v2) 
+    {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+
+    // Векторное произведение 
+    Vector crossProduct(const Vector& v1, const Vector& v2) 
+    {
+        return Vector
+            (v1.y * v2.z - v1.z * v2.y, // на выходе получаем вектор
+            v1.z * v2.x - v1.x * v2.z,
+            v1.x * v2.y - v1.y * v2.x);
+    }
+    // Смешанное произведение (для 3 векторов)
+    double mixedProduct(const Vector& v1, const Vector& v2, const Vector& v3)
+    {
+        return v1.x * (v2.y * v3.z - v3.y * v2.z) // расписываем определитель и получаем число 
+            - v1.y*(v2.x * v3.z - v3.x * v2.z) 
+            + v1.z * (v2.x * v3.y - v3.x * v2.y);
+    }
+
+    // Длина вектора
+    double length(const Vector& v) 
+    {
+        return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    }
+
+    // Проверка коллинеарности векторов
+    bool areCollinear(const Vector& v1, const Vector& v2) 
+    {
+        // Векторы коллинеарны, если их векторное произведение равно нулевому вектору
+        return crossProduct(v1, v2).x == 0 && crossProduct(v1, v2).y == 0 && crossProduct(v1, v2).z == 0;
+    }
+
+    // Проверка компланарности векторов
+    bool areCoplanar(const Vector& v1, const Vector& v2, const Vector& v3) 
+    {
+        // Векторы компланарны, если их смешанное произведение равно нулю
+        return dotProduct(crossProduct(v1, v2), v3) == 0;
+    }
+
+    // Расстояние между векторами
+    double distance(const Vector& v1, const Vector& v2) 
+    {
+        return length(v2 - v1);
+    }
+
+    // Нахождение угла между векторами (в радианах)
+    double angle(const Vector& v1, const Vector& v2) 
+    {
+        double cosTheta = dotProduct(v1, v2) / (length(v1) * length(v2));
+        return std::acos(cosTheta);
+    }
+}
---
+++ lab_2/lab_2/lab_2.cpp
@@ -0,0 +1,64 @@
+﻿#include "Vector.h"
+
+//using namespace vector_func;
+
+int main() 
+{
+    SetConsoleOutputCP(1251); // Для русского языка в консоли
+
+    // Пример использования классов и операций
+    double x1, y1, z1, x2, y2, z2, x3, y3, z3, x7, y7, z7;
+    std::cout << "Введите значения для точки p1 через пробел: ";
+    std::cin >> x1 >> y1 >> z1;
+    std::cout << "Введите значения для точки p2 через пробел: ";
+    std::cin >> x2 >> y2 >> z2;
+    vector_func::Point p1(x1, y1, z1);
+    vector_func::Point p2(x2, y2, z2);
+    vector_func::Vector v1(p1, p2);
+    std::cout << "Вектор, который строится по этим точкам(p1, p2) имеет координаты: x = " 
+        << v1.getX() << " y = " << v1.getY() << " z = " << v1.getZ() << '\n';
+
+    std::cout << "Введите значения для вектора v2 через пробел: ";
+    std::cin >> x3 >> y3 >> z3;
+    vector_func::Vector v2(x3, y3, z3);
+    vector_func::Vector v3 = v2 + v1;
+
+    std::cout << "Сложение векторов v1 + v2, где координаты x = " << 
+        v3.getX() << " y = " << v3.getY() << " z = " << v3.getZ() << '\n';
+    vector_func::Vector v4 = v2 - v1;
+
+    std::cout << "Вычитание векторов v2 - v1, где координаты x = " <<
+        v4.getX() << " y = " << v4.getY() << " z = " << v4.getZ() << '\n';
+    vector_func::Vector v5 = vector_func::normalize(v1);
+    vector_func::Vector v6 = -v1;
+    std::cout << "Обратный вектор для v1: x = " << 
+        v6.getX() << " y = " << v6.getY() << " z = " << v6.getZ() << '\n';
+
+    std::cout << "Введите значения для вектора v7 через пробел: ";
+    std::cin >> x7 >> y7 >> z7;
+    vector_func::Vector v7(x7, y7, z7);
+
+    if (vector_func::areCollinear(v2, v7))
+    {
+        std::cout << "Вектора v2 и v7 коллениарны!\n";
+    }
+    else 
+    {
+        std::cout << " Вектора v2 и v7 НЕ коллениарны!\n";
+    }
+
+    if (vector_func::areCoplanar(v1, v2, v7))
+    {
+        std::cout << "Вектора v1, v2 и v7 компланарны!\n";
+    }
+    else
+    {
+        std::cout << " Вектора v1, v2 и v7 НЕ компланарны!\n";
+    }
+
+    std::cout << "Расстояние между вектором v2 и v7 = " << vector_func::distance(v2, v7) << '\n';
+
+    std::cout << "Угол между векторами v2 и v7 = " << vector_func::angle(v2, v7);
+
+    return 0;
+}
