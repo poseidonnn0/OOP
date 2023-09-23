559b481253d492e0239bd95e455ad99ba801a01e
Vitaliy <79005626141@mail.ru>
2023-09-23 21:33:12 +0200

    +

--- lab_2/lab_2/Vector.cpp
+++ lab_2/lab_2/Vector.cpp
@@ -27,23 +27,23 @@ namespace vector_func
     }
 
     // Построение единичного вектора
-    Vector normalize(const Vector& v) 
+    Vector normalize(const Vector& v)
     {
         double length = std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
         return Vector(v.x / length, v.y / length, v.z / length);
     }
 
     // Скалярное произведение
-    double dotProduct(const Vector& v1, const Vector& v2) 
+    double dotProduct(const Vector& v1, const Vector& v2)
     {
         return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
     }
 
     // Векторное произведение 
-    Vector crossProduct(const Vector& v1, const Vector& v2) 
+    Vector crossProduct(const Vector& v1, const Vector& v2)
     {
         return Vector
-            (v1.y * v2.z - v1.z * v2.y, // на выходе получаем вектор
+        (v1.y * v2.z - v1.z * v2.y, // на выходе получаем вектор
             v1.z * v2.x - v1.x * v2.z,
             v1.x * v2.y - v1.y * v2.x);
     }
@@ -51,38 +51,38 @@ namespace vector_func
     double mixedProduct(const Vector& v1, const Vector& v2, const Vector& v3)
     {
         return v1.x * (v2.y * v3.z - v3.y * v2.z) // расписываем определитель и получаем число 
-            - v1.y*(v2.x * v3.z - v3.x * v2.z) 
+            - v1.y * (v2.x * v3.z - v3.x * v2.z)
             + v1.z * (v2.x * v3.y - v3.x * v2.y);
     }
 
     // Длина вектора
-    double length(const Vector& v) 
+    double length(const Vector& v)
     {
         return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
     }
 
     // Проверка коллинеарности векторов
-    bool areCollinear(const Vector& v1, const Vector& v2) 
+    bool areCollinear(const Vector& v1, const Vector& v2)
     {
         // Векторы коллинеарны, если их векторное произведение равно нулевому вектору
         return crossProduct(v1, v2).x == 0 && crossProduct(v1, v2).y == 0 && crossProduct(v1, v2).z == 0;
     }
 
     // Проверка компланарности векторов
-    bool areCoplanar(const Vector& v1, const Vector& v2, const Vector& v3) 
+    bool areCoplanar(const Vector& v1, const Vector& v2, const Vector& v3)
     {
         // Векторы компланарны, если их смешанное произведение равно нулю
         return dotProduct(crossProduct(v1, v2), v3) == 0;
     }
 
     // Расстояние между векторами
-    double distance(const Vector& v1, const Vector& v2) 
+    double distance(const Vector& v1, const Vector& v2)
     {
         return length(v2 - v1);
     }
 
     // Нахождение угла между векторами (в радианах)
-    double angle(const Vector& v1, const Vector& v2) 
+    double angle(const Vector& v1, const Vector& v2)
     {
         double cosTheta = dotProduct(v1, v2) / (length(v1) * length(v2));
         return std::acos(cosTheta);
